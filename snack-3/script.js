a0132c81ea430a848a211f212e20ddd88845970b
DarioConiglione <dariocng13@gmail.com>
2025-10-22 14:45:32 +0100

    snack 3 done

--- snack-3/script.js
+++ snack-3/script.js
@@ -1,4 +1,11 @@
 const numbers = [2, 8, 4, 7, 2, 87];
 
 // Crea un nuovo array in cui tutti i numeri siano incrementati di 1
+const newNums = [];
+
+numbers.forEach(number => {
+  newNums.push(number + 1);
+});
+
+console.log(newNums);
 // Risultato: [3, 9, 5, 8, 3, 88]
